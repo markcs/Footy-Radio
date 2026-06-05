011eccb2fc9ebf80707fa2688dfb83203c42f106
Mark Campbell-Smith <mcampbellsmith@gmail.com>
2026-06-05 03:12:30 +0000

    chore: update stationsURL to new path

--- app/src/main/java/com/fethica/swiftradio/Config.kt
+++ app/src/main/java/com/fethica/swiftradio/Config.kt
@@ -6,7 +6,7 @@ object Config {
     val gradientColor: Color = Color.White
 
     const val useLocalStations = false
-    const val stationsURL = "https://markcs.github.io/Footy-Radio/stations.json"
+    const val stationsURL = "https://markcs.github.io/Footy-Radio/stations/stations.json"
 
     const val hideNextPreviousButtons = false
     const val enableSearch = true // Toggle to show/hide search bar on the Stations Screen
